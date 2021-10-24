14a1517a8b5db105cb4bc15eddadc8530ee1ed95
Ricardo Ducci Popi <ducci.popi@gmail.com>
2021-10-23 21:05:30 -0300

    RemindersActivity End-to-end test added

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,16 +1,34 @@
 package com.udacity.project4
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PointOfInterest
+import com.udacity.project4.authentication.Authenticator
+import com.udacity.project4.authentication.FakeAuthenticationController
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.monitorActivity
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.not
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -27,6 +45,10 @@ class RemindersActivityTest :
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    private val fakePoi = PointOfInterest(LatLng(11.00, 11.00), "Fake_POI_ID", "Fake POI Title")
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -51,6 +73,8 @@ class RemindersActivityTest :
             }
             single { RemindersLocalRepository(get()) as ReminderDataSource }
             single { LocalDB.createRemindersDao(appContext) }
+            single { FakeAuthenticationController(true) as Authenticator }
+            single { listOf(fakePoi) }
         }
         //declare a new koin module
         startKoin {
@@ -63,6 +87,79 @@ class RemindersActivityTest :
         runBlocking {
             repository.deleteAllReminders()
         }
+    }
+
+    @After
+    fun finish() {
+        runBlocking {
+            repository.deleteAllReminders()
+        }
+    }
+
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    @Test
+    fun tryToCreateReminder() {
+        // Reminder to be saved
+        val reminder = ReminderDataItem(
+            "Test Title 1", "Test Description 1",
+            fakePoi.name, fakePoi.latLng.latitude, fakePoi.latLng.longitude
+        )
+
+        // Start up Reminders screen.
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // Click add reminder FAB
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        // Check if Save Reminder is loaded, clean and with hints
+        onView(withId(R.id.reminderTitle)).check(matches(isDisplayed()))
+        onView(withId(R.id.reminderTitle)).check(matches(withHint(R.string.reminder_title)))
+        onView(withId(R.id.reminderTitle)).check(matches(withText("")))
+        onView(withId(R.id.reminderDescription)).check(matches(isDisplayed()))
+        onView(withId(R.id.reminderDescription)).check(matches(withHint(R.string.reminder_desc)))
+        onView(withId(R.id.reminderDescription)).check(matches(withText("")))
+        onView(withId(R.id.selectLocation)).check(matches(isDisplayed()))
+        onView(withId(R.id.selectLocation)).check(matches(withText(R.string.reminder_location)))
+        onView(withId(R.id.selectedLocation)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.selectedLocation)).check(matches(withText("")))
+
+        // Set title and description
+        onView(withId(R.id.reminderTitle)).perform(replaceText(reminder.title))
+        onView(withId(R.id.reminderDescription)).perform(replaceText(reminder.description))
+
+        // Click select location
+        onView(withId(R.id.selectLocation)).perform(click())
+
+        // Check map is loaded
+        onView(withId(R.id.map)).check(matches(isDisplayed()))
+
+        // Click select location FAB
+        onView(withId(R.id.savePoi)).perform(click())
+
+        // Check if select location name is displayed
+        onView(withId(R.id.selectedLocation)).check(matches(isDisplayed()))
+        onView(withId(R.id.selectedLocation)).check(matches(withText(fakePoi.name)))
+
+        // Click to save reminder
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        // Check if it was correctly displayed
+        onView(withId(R.id.reminderssRecyclerView)).check(matches(isDisplayed()))
+        onView(withId(R.id.reminderssRecyclerView)).check(matches(hasDescendant((withText(reminder.title)))))
+        onView(withId(R.id.reminderssRecyclerView)).check(matches(hasDescendant((withText(reminder.description)))))
+        onView(withId(R.id.reminderssRecyclerView)).check(matches(hasDescendant((withText(reminder.location)))))
+
+        activityScenario.close()
     }
 
 
