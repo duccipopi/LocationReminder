85077bdd8d8f7d6c0097d7dde67dfec33786d395
Ricardo Ducci Popi <ducci.popi@gmail.com>
2021-10-25 22:11:29 -0300

    Fix request for enable location services

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -1,16 +1,16 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.app.Activity
+import android.content.Intent
 import android.content.IntentSender
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.LocationSettingsRequest
-import com.google.android.gms.location.LocationSettingsResponse
-import com.google.android.gms.location.SettingsClient
+import com.google.android.gms.location.*
 import com.google.android.gms.tasks.Task
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
@@ -51,35 +51,44 @@ class SaveReminderFragment : BaseFragment() {
         }
 
         binding.saveReminder.setOnClickListener {
-            val title = _viewModel.reminderTitle.value
-            val description = _viewModel.reminderDescription.value
-            val location = _viewModel.reminderSelectedLocationStr.value
-            val latitude = _viewModel.latitude.value
-            val longitude = _viewModel.longitude.value
+            tryToSaveReminder()
+        }
+    }
 
-            val reminder = ReminderDataItem(
-                title, description, location, latitude, longitude
-            )
+    private fun tryToSaveReminder() {
+        val title = _viewModel.reminderTitle.value
+        val description = _viewModel.reminderDescription.value
+        val location = _viewModel.reminderSelectedLocationStr.value
+        val latitude = _viewModel.latitude.value
+        val longitude = _viewModel.longitude.value
 
-            if (allPermissionsGranted(LOCATION_BACKGROUND_PERMISSIONS)
-                && anyPermissionsGranted(LOCATION_FOREGROUND_PERMISSIONS)
-            ) {
+        val reminder = ReminderDataItem(
+            title, description, location, latitude, longitude
+        )
 
-                processIfDeviceLocationIsOn {
-                    addGeofence(requireContext(), reminder)
-                    _viewModel.validateAndSaveReminder(reminder)
-                }
+        if (allPermissionsGranted(LOCATION_BACKGROUND_PERMISSIONS)
+            && anyPermissionsGranted(LOCATION_FOREGROUND_PERMISSIONS)
+        ) {
 
-            } else {
-                val permission = LOCATION_BACKGROUND_PERMISSIONS + LOCATION_FOREGROUND_PERMISSIONS
-                requestMissingPermissions(permission)
+            processIfDeviceLocationIsOn {
+                addGeofence(requireContext(), reminder)
+                _viewModel.validateAndSaveReminder(reminder)
             }
+
+        } else {
+            val permission = LOCATION_BACKGROUND_PERMISSIONS + LOCATION_FOREGROUND_PERMISSIONS
+            requestMissingPermissions(permission)
         }
     }
 
     private fun processIfDeviceLocationIsOn(function: () -> Unit) {
 
-        val builder = LocationSettingsRequest.Builder()
+        val builder =
+            LocationSettingsRequest.Builder().addLocationRequest(
+                LocationRequest.create().apply {
+                    priority = LocationRequest.PRIORITY_LOW_POWER
+                }
+            )
 
         val client: SettingsClient = LocationServices.getSettingsClient(requireActivity())
         val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
@@ -91,9 +100,9 @@ class SaveReminderFragment : BaseFragment() {
                 try {
                     // Show the dialog by calling startResolutionForResult(),
                     // and check the result in onActivityResult().
-                    exception.startResolutionForResult(
-                        requireActivity(),
-                        REQUEST_CHECK_SETTINGS
+                    startIntentSenderForResult(
+                        exception.resolution.intentSender, REQUEST_CHECK_SETTINGS,
+                        null, 0, 0, 0, null
                     )
                 } catch (sendEx: IntentSender.SendIntentException) {
                     // Ignore the error.
@@ -105,6 +114,39 @@ class SaveReminderFragment : BaseFragment() {
             if (it.isSuccessful) function()
         }
 
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_PERMISSION_REQUEST_CODE) {
+            val foreground = permissions.filter { LOCATION_FOREGROUND_PERMISSIONS.contains(it) }
+            val anyForeground =
+                foreground.isEmpty() || foreground.any { grantResults[permissions.indexOf(it)] == PackageManager.PERMISSION_GRANTED }
+
+            val background = permissions.filter { LOCATION_BACKGROUND_PERMISSIONS.contains(it) }
+            val allBackground =
+                background.isEmpty() || background.all { grantResults[permissions.indexOf(it)] == PackageManager.PERMISSION_GRANTED }
+
+            if (anyForeground && allBackground) {
+                tryToSaveReminder()
+            } else {
+                _viewModel.showSnackBarInt.value = R.string.permission_denied_explanation
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                tryToSaveReminder()
+            } else {
+                _viewModel.showSnackBarInt.value = R.string.location_required_error
+            }
+        }
     }
 
     override fun onDestroy() {
